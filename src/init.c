f1732cbc101fe84eabb1abd4f4be1e2ae322adf6
mmiguelo <marco_miguelote@hotmail.com>
2025-05-04 23:50:34 +0100

    added condition to assign_forks

--- src/init.c
+++ src/init.c
@@ -12,7 +12,47 @@
 
 #include "philo.h"
 
+static void	assign_forks(t_philo *philo, t_fork *forks, int position)
+{
+	int	philo_nbr;
+
+	philo_nbr = philo->table->philo_nbr;
+	if (philo->id % 2)
+	{
+		philo->right_fork = &forks[position];
+		philo->left_fork = &forks[(position + 1) % philo_nbr];
+	}
+}
+
+static void	philo_init(t_table *table)
+{
+	int		i;
+	t_philo	*philo;
+
+	i = -1;
+	while (++i < table->philo_nbr)
+	{
+		philo = table->philos + i;
+		philo->id = i + 1;
+		philo->full = false;
+		philo->meal_counter = 0;
+		philo->table = table;
+		assign_forks(philo, table->forks, i);
+	}
+}
+
 void	data_init(t_table *table)
 {
+	int	i;
+
+	i = -1;
 	table->end_simulation = false;
+	table->philos = safe_malloc(sizeof(t_philo) * table->philo_nbr);
+	table->forks = safe_malloc(sizeof(t_fork) * table->philo_nbr);
+	while (++i < table->philo_nbr)
+	{
+		safe_mutex_handle(&table->forks[i].fork, INIT);
+		table->forks[i].fork_id = i;
+	}
+	philo_init(table);
 }
