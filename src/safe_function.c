f11a83fe73bf62391be58adbcae00a3501094d3f
mmiguelo <marco_miguelote@hotmail.com>
2025-05-04 22:36:56 +0100

    add safe_mutex_handle function

--- src/safe_function.c
+++ src/safe_function.c
@@ -21,3 +21,23 @@ void	*safe_malloc(size_t bytes)
 		error_msg("Error with malloc\n");
 	return (ret);
 }
+
+static void	handle_mutex_error(int status, t_opcode opcode)
+{
+	if (status == 0)
+		return
+}
+
+void	safe_mutex_handle(t_mtx *mutex, t_opcode opcode)
+{
+	if (LOCK == opcode)
+		pthread_mutex_lock(mutex);
+	else if (UNLOCK == opcode)
+		pthread_mutex_unlock(mutex);
+	else if (INIT == opcode)
+		pthread_mutex_init(mutex, NULL);
+	else if (DESTROY == opcode)
+		pthread_attr_destroy(mutex);
+	else
+		error_msg("Wrong opcode for mutex handle");
+}
