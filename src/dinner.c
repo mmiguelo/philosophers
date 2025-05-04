9d5fb27c28aacfc067db7c87ce38f91f94ef835d
mmiguelo <marco_miguelote@hotmail.com>
2025-05-05 00:05:23 +0100

    added dinner start conditions

--- src/dinner.c
+++ src/dinner.c
@@ -12,3 +12,19 @@
 
 #include "philo.h"
 
+void	dinner_start(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	if (table->nbr_limit_meals == 0)
+		return ;
+	else if (table->philo_nbr == 1)
+		;
+	else
+	{
+		while (++i < table->philo_nbr)
+			safe_thread_handle(&table->philos[i].thread_id, dinner_simulation,
+					&table->philos[i], CREATE);
+	}
+}
