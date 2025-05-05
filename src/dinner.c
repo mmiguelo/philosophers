11e10911168109264e62ee264f6ff13167fae5a2
mmiguelo <marco_miguelote@hotmail.com>
2025-05-05 15:52:17 +0100

    added function eat to file

--- src/dinner.c
+++ src/dinner.c
@@ -12,6 +12,40 @@
 
 #include "philo.h"
 
+static void	eat(t_philo *philo)
+{
+	safe_mutex_handle(&philo->first_fork->fork, LOCK);
+	write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
+	safe_mutex_handle(&philo->second_fork->fork, LOCK);
+	write_status(TAKE_SECOND_FORK, philo, DEBUG_MODE);
+	set_long(&philo->philo_mutex, &philo->last_meal_time, gettime(MILISECOND));
+	philo->meal_counter++;
+	precise_usleep(philo->table->time_to_eat, philo->table);
+	if (philo->table->nbr_limit_meals > 0
+		&& philo->meal_counter == philo->table->nbr_limit_meals)
+		set_bool(&philo->philo_mutex, &philo->full, true);
+	safe_mutex_handle(&philo->first_fork->fork, UNLOCK);
+	safe_mutex_handle(&philo->first_fork->fork, UNLOCK);
+}
+
+void	*dinner_simulation(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	wait_all_threads(philo->table);
+	while(!simulation_finished(philo->table))
+	{
+		if (philo->full)
+			break ;
+		eat(philo);
+		write_status(SLEEPING, philo, DEBUG_MODE);
+		precise_usleep(philo->table->time_to_sleep, philo->table);
+		think(philo);
+	}
+	return (NULL);
+}
+
 void	dinner_start(t_table *table)
 {
 	int	i;
@@ -27,4 +61,9 @@ void	dinner_start(t_table *table)
 			safe_thread_handle(&table->philos[i].thread_id, dinner_simulation,
 				&table->philos[i], CREATE);
 	}
+	table->start_simulation = gettime(MILISECOND);
+	set_bool(&table->table_mutex, &table->all_threads_ready, true);
+	i = -1;
+	while (++i < table->philo_nbr)
+		safe_thread_handle(&table->philos[i].thread_id, NULL, NULL, JOIN);
 }
