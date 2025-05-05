845dfecfbd6529addf62a0ec4038cc1ba0b06b15
mmiguelo <marco_miguelote@hotmail.com>
2025-05-05 15:24:39 +0100

    added function write

--- src/write.c
+++ src/write.c
@@ -12,7 +12,29 @@
 
 #include "philo.h"
 
-void	write_status()
+void	write_status(t_status status, t_philo *philo, bool debug)
 {
+	long	elapsed;
 
+	elapsed = gettime(MILISECOND) - philo->table->start_simulation;
+	if (philo->full)
+		return ;
+	safe_mutex_handle(&philo->table->write_mutex, LOCK);
+	if (debug)
+		write_status_debug(status, philo, elapsed);
+	else
+	{
+		if ((TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status)
+			&& !simulation_finished(philo->table))
+			printf(W"%-6ld"RST" %d has taken a fork\n", elapsed, philo->id);
+		else if (EATING == status && !simulation_finished(philo->table))
+			printf(W"%-6ld"C" %d is eating\n"RST, elapsed, philo->id);
+		else if (SLEEPING == status && !simulation_finished(philo->table))
+			printf(W"%-6ld"RST" %d is sleeping\n", elapsed, philo->id);
+		else if (THINKING == status && !simulation_finished(philo->table))
+			printf(W"%-6ld"RST" %d is thinking\n", elapsed, philo->id);
+		else if (DIED == status)
+			printf(RED"%-6ld %d died\n"RST, elapsed, philo->id);
+	}
+	safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
 }
